4e31e14c7ca9c6f271f4cc084e35c9c85726ceaa
Tomas Durkin <tdurkin1@gmail.com>
2020-12-22 23:56:38 -0800

    Setup Environment
    -Setup Simple Environment
    -Added XR Rig
    -Added Locomotion, Snapturn, teleportation areas
    -created targetMovement script
    -created canon script

---
+++ Snowball-Bootcamp-VR/Assets/Cannon.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannon : MonoBehaviour
+{
+    [SerializeField] float turnSpeed = 4f;
+    [SerializeField] float senseRange = 5f;
+
+    private Camera mainCamera;
+    void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distToTarget = Vector3.Distance(transform.position, mainCamera.transform.position);
+        if(distToTarget < senseRange)
+        {
+            FacePlayer();
+        }
+        
+    }
+
+    private void FacePlayer()
+    {
+        Vector3 lookDirection = (mainCamera.transform.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(lookDirection);
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, turnSpeed * Time.deltaTime);
+    }
+}
---
+++ Snowball-Bootcamp-VR/Assets/Scripts/TargetMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetMovement : MonoBehaviour
+{
+    Vector3 sinePos;
+    float time;
+    public float targetSpeed = 0.5f;
+    public float movementAmplitude = .025f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        sinePos.z = Mathf.Sin(time * targetSpeed) * movementAmplitude;
+        transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + sinePos.z); ;
+
+
+    }
+}
