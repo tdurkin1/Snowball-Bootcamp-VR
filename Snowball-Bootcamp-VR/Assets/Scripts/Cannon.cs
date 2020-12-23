c590e715482f9c716a4fc9ddf74cdacb779b2063
Tomas Durkin <tdurkin1@gmail.com>
2020-12-23 03:21:40 -0800

    Added shooting to SnowballCannon

--- Snowball-Bootcamp-VR/Assets/Scripts/Cannon.cs
+++ Snowball-Bootcamp-VR/Assets/Scripts/Cannon.cs
@@ -2,13 +2,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Cannon : MonoBehaviour
 {
     [SerializeField] float turnSpeed = 4f;
     [SerializeField] float senseRange = 5f;
+    [SerializeField] float cannonBallSpeed = 10f;
+    [SerializeField] float shootInterval = 3f;
+    [SerializeField] GameObject cannonBall;
+    [SerializeField] Transform barrelPos;
 
     private Camera mainCamera;
+    private float nextBallTime = 0f;
 
     void Start()
     {
@@ -21,6 +27,7 @@ public class Cannon : MonoBehaviour
         if(distToTarget < senseRange)
         {
             FacePlayer();
+            ShootPlayer();
         }        
     }
 
@@ -29,5 +36,24 @@ public class Cannon : MonoBehaviour
         Vector3 lookDirection = (mainCamera.transform.position - transform.position).normalized;
         Quaternion lookRotation = Quaternion.LookRotation(lookDirection);
         transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, turnSpeed * Time.deltaTime);
+    }
+
+    private void ShootPlayer()
+    {
+        if(Time.time > nextBallTime)
+        {
+            //shoot player
+            GameObject newBall = (GameObject)Instantiate(cannonBall);
+            if (newBall != null)
+            {
+                newBall.transform.position = barrelPos.position;
+                newBall.transform.rotation = barrelPos.rotation;
+                Rigidbody rb = newBall.GetComponent<Rigidbody>();
+                rb.angularVelocity = Vector3.zero;
+                rb.velocity = barrelPos.forward * cannonBallSpeed;
+            }
+            nextBallTime = Time.time + shootInterval;
+        }
+        
     }
 }
